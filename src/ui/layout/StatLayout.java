15e2a41fb7c64fb794ef8f110f60b7ecbebdc3f0
Fabricio-Tomas <78918951+Fabricio-Tomas@users.noreply.github.com>
2021-03-27 21:11:25 -0300

    Añadida estadistica extra
    Solución bug-b#2: Una estadistica de la lista no era mostrada correctamente

--- src/ui/layout/StatLayout.java
+++ src/ui/layout/StatLayout.java
@@ -7,18 +7,19 @@ public class StatLayout extends LayoutAdapter {
 	@Override
 	public void layoutContainer(Container c) {
 
-		int n = c.getComponentCount() - 1;
+		int N = c.getComponentCount();
+//		int n = c.getComponentCount() - 1;
 
 		int H = c.getHeight();
 		int W = c.getWidth();
 
-		if (n == 1) {
+		if (N == 2) {
 			c.getComponent(0).setBounds(0, 0, W, (int) (H * 0.2));
 			c.getComponent(1).setBounds(0, (int) (H * 0.1), W, (int) (H * 0.8));
 			return;
 		}
 
-		int H2 = H / n;
+		int H2 = H / N;
 		int h = (int) (H2 * 0.9);
 		int w = (int) (W * 0.9);
 
@@ -26,8 +27,9 @@ public class StatLayout extends LayoutAdapter {
 		int y = H2 / 2 - h / 2;
 
 		c.getComponent(0).setBounds(x, y, w, h);
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i < N; i++) {
 
+			System.out.println(i);
 			y += H2;
 			c.getComponent(i).setBounds(x, y, w, h);
 
